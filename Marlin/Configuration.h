2f9667a7e51bf2630ab2aef8a8d58bde7016ee99
Felix Na <felix.na70@gmail.com>
2020-06-20 20:30:04 +0200

    tune BLtouch parameters, disable stealthChop for E

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -966,7 +966,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -1, -24, -0.78 }
+#define NOZZLE_TO_PROBE_OFFSET { -1, -24, -1.475 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1261,7 +1261,7 @@
 #if EITHER(AUTO_BED_LEVELING_LINEAR, AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 5
+  #define GRID_MAX_POINTS_X 3
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Probe along the Y axis, advancing X after each column
